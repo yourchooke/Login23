b51686e29eceba3bd10f15ee61ff8f38c3a40265
Olga Yurchuk <olga.yurchuk@xored.com>
2022-06-09 16:31:44 +0700

    Initial Commit

---
+++ Login23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login23
+//
+//  Created by Olga Yurchuk on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
