37b9443fd2e2e15ba0ccf4d49c5a3a05bd636bd4
Olga Yurchuk <olga.yurchuk@xored.com>
2022-06-21 21:07:29 +0700

    Model added. Minor fix in main storyboard

--- Login23/Models/User.swift
+++ Login23/Models/User.swift
@@ -5,44 +5,59 @@
 //  Created by Olga Yurchuk on 17.06.2022.
 //
 
-class User {
+struct User {
     let user: String
     let pass: String
     let person: Person
-
-    init(
-        user: String,
-        pass: String,
-        name: String,
-        surname: String,
-        sex: Sex,
-        age: Int
-    )
-    {
-        person = Person.init(name: name, surname: surname, sex: sex, age: age)
-        self.user = user
-        self.pass = pass
+    
+    static func getUsers() -> [User] {
+        [
+            User(
+                user: "User",
+                pass: "Pass",
+                person: Person(
+                    name: "Оля",
+                    age: 32,
+                    sex: .female,
+                    children: ["Алёша", "Лёня"],
+                    hobbies: ["Вязание", "Сериалы", "Instagram", "Психология"]
+                )
+            ),
+            User(
+                user: "User1",
+                pass: "Pass1",
+                person: Person(
+                    name: "Саша",
+                    age: 32,
+                    sex: .male,
+                    children: ["Лёха", "Ленни"],
+                    hobbies: ["Сериалы", "Counter Strike", "Качалка"]
+                )
+            )
+        ]
+    }
+    
+    static func getEmptyUser() -> User {
+        User(
+            user: "",
+            pass: "",
+            person: Person(
+                name: "",
+                age: 0,
+                sex: .male,
+                children: [],
+                hobbies: []
+            )
+        )
     }
 }
 
-class Person {
+struct Person {
     let name: String
-    let surname: String
+    let age: Int
     let sex: Sex
-    var age = 0
-    var partner: [Person]
-    var family: [Person]
-    var hobbies: [String]
-    
-    init(name: String, surname: String, sex: Sex, age: Int){
-        self.name = name
-        self.surname = surname
-        self.sex = sex
-        self.age = age
-        partner = []
-        family = []
-        hobbies = []
-    }
+    let children: [String]
+    let hobbies: [String]
 }
 
 enum Sex {
